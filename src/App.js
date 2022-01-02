07e636f64988a7a9ee25fd8a43458aa0cf47e2e0
yayakyogi <yayaktaka@gmail.com>
2022-01-02 23:23:10 +0700

    add mapping json review

--- src/App.js
+++ src/App.js
@@ -3,13 +3,61 @@ import "./App.css";
 function App() {
   return (
     <div className="parent-box">
-      <Foto />
-      <Description
-        category="SNEAKER"
-        name="Sepatu Gaul"
-        price="150.000"
-        isDiskon={false}
-      />
+      <div className="content-detail">
+        <Foto />
+        <Description
+          category="SNEAKER"
+          name="Sepatu Gaul"
+          price="150.000"
+          isDiskon={false}
+        />
+      </div>
+      <Review />
+    </div>
+  );
+}
+
+function Review() {
+  const review = [
+    {
+      id: 1,
+      name: "Yayak",
+      comment:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+      img: "https://images.pexels.com/photos/2216542/pexels-photo-2216542.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    },
+    {
+      id: 2,
+      name: "Taka",
+      comment:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+      img: "https://images.pexels.com/photos/899954/pexels-photo-899954.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    },
+    {
+      id: 3,
+      name: "Ginantaka",
+      comment:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+      img: "https://images.pexels.com/photos/654690/pexels-photo-654690.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    },
+  ];
+
+  const listReview = review.map((list) => {
+    return (
+      <div key={list.id} className="content-review">
+        <img src={list.img} alt="img-dummy" />
+        <div className="review-information">
+          <h6>{list.name}</h6>
+          <p>{list.comment}</p>
+        </div>
+      </div>
+    );
+  });
+  return (
+    <div className="review">
+      <h5>Review</h5>
+      {listReview}
+      <div style={{ height: 100 }}></div>
     </div>
   );
 }
