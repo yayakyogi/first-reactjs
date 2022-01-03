46404a2903d75c54c7b581d2ddd4bac2dc0329d4
yayakyogi <yayaktaka@gmail.com>
2022-01-03 07:18:49 +0700

    try fetch api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import "./App.css";
-import Photo from "./Components/FirstReactBWA/Photo";
-import Description from "./Components/FirstReactBWA/Description";
-import Reviews from "./Components/FirstReactBWA/Reviews";
+import FetchApi from "./FetchApi";
 
 function App() {
-  return (
-    <div className="parent-box">
-      <div className="content-detail">
-        <Photo />
-        <Description
-          category="SNEAKER"
-          name="Vans KW Super"
-          price={150000}
-          isDiskon={false}
-        />
-      </div>
-      <Reviews />
-    </div>
-  );
+  return <FetchApi />;
 }
 
 export default App;
