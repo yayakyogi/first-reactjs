d0364256e42576c375b77ed8d03778c821bed915
yayakyogi <yayaktaka@gmail.com>
2022-01-03 07:42:48 +0700

    change to reack hooks

--- src/FetchApi/index.js
+++ src/FetchApi/index.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 
-export default class FetchApi extends Component {
+// * CLASS COMPPONENT
+class FetchApiClass extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -70,5 +71,66 @@ export default class FetchApi extends Component {
         </div>
       );
     }
+  }
+}
+
+// * HOOKS
+export default function FetchApiHooks() {
+  const [error, setError] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return (
+      <div>
+        <h1>Error: {error.message}</h1>
+      </div>
+    );
+  } else if (!isLoaded) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className="fetch-api">
+        <ul className="wrap">
+          {items.map((result) => {
+            return (
+              <li key={result.id} className="card">
+                <div className="profile">
+                  <h5>Profil</h5>
+                  <p>Nama: {result.name}</p>
+                  <p>Email: {result.email}</p>
+                  <p>Webise: {result.website}</p>
+                </div>
+                <div className="address">
+                  <h5>Alamat</h5>
+                  <p>Kota: {result.address.city}</p>
+                  <p>Jalan: {result.address.street}</p>
+                  <p>No rumah: {result.address.suite}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
 }
